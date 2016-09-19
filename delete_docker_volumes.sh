2d6082f8ff5e1932246bd73821699c95bc7b6681
Argha C <argha@soundcloud.com>
2016-09-19 18:53:19 +0200

    Delete only dangling volumes.

--- delete_docker_volumes.sh
+++ delete_docker_volumes.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -e
 
-volumes=$(docker volume ls | awk '{if(NR>1) print $2}')
+volumes=$(docker volume ls -f dangling=true | awk '{if(NR>1) print $2}')
 
 if [ ${#volumes[@]} -le 1 ];then
   echo No volumes eligible for deletion.
@@ -9,5 +9,5 @@ else
   for vol in "${volumes[@]}"; do
     docker volume rm ${vol}
   done
+  echo Successfully deleted ${#volumes[@]} orphaned volume\(s\)!
 fi
-echo Successfully deleted ${#volumes[@]} orphaned volume\(s\)!
